0d9a0c5208e01697b9252bd5bd4670313ff2a8cd
Edouard de Romrée <deromree.edouard@gmail.com>
2023-02-22 14:41:31 +0100

    refactorisation

--- js/script.js
+++ js/script.js
@@ -1,114 +1,113 @@
-const input= document.querySelector("input")
-const button= document.querySelector("button")
-let div=document.createElement("div")
+const input = document.querySelector("input")
+const button = document.querySelector("button")
+let div = document.createElement("div")
+let now = new Date()
+const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+const divGraph = document.querySelector(".myGraph")
+let arrDay = []
+let arrTemp = []
+let arrFeels = []
+
 document.body.appendChild(div)
-let now = new Date ()
-const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-const divGraph= document.querySelector(".myGraph")
-let arrDay= []
-let arrTemp=[]
-let arrFeels=[]
 
+function appendChild(element, parent) {
+  parent.appendChild(element)
+}
 
-button.addEventListener('click',()=>
-{
-    div.innerHTML=""
-    let userLocation= input.value
-    let fetchWeather=(userLocation) => fetch('https://api.openweathermap.org/data/2.5/forecast?q='+userLocation+'&appid=e69c960a61169de7a06b2a5805d637b0&units=metric')
-    fetchWeather(userLocation)
-    .then((response=> response.json()))
-    .then((json)=>{  
-        localStorage.setItem("data",JSON.stringify(json))
-        let nameCity= json.city.name
-        h2= document.createElement("h2")
-        div.appendChild(h2)
-        h2.textContent=nameCity
-        let list= json.list
+function addClassName(element, className) {
+  element.classList.add(className)
+}
+
+button.addEventListener('click', () => {
+  div.innerHTML = ""
+  let userLocation = input.value
+  let fetchWeather = (userLocation) => fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + userLocation + '&appid=e69c960a61169de7a06b2a5805d637b0&units=metric')
+  fetchWeather(userLocation)
+    .then((response => response.json()))
+    .then((json) => 
+    {
+      localStorage.setItem("data", JSON.stringify(json))
+      let nameCity = json.city.name
+      h2 = document.createElement("h2")
+      div.appendChild(h2)
+      h2.textContent = nameCity
+      let list = json.list
+
+      let divCarte = document.createElement("div")
+      divCarte.classList.add("carte")
+      appendChild(divCarte, div)
+
+      for (elem of list) 
+    {
+        let ensemble = document.createElement("div")
+        ensemble.classList.add("heuresetjours")
+        appendChild(ensemble, divCarte)
+
+        //image
+        let des = document.createElement("div")
+        des.classList.add("image")
+        let description = document.createElement("p")
+        addClassName(description, "description")
+        description.textContent = elem.weather[0].description
+        let image = document.createElement("img")
+        image.src = "http://openweathermap.org/img/wn/" + elem.weather[0].icon + "@2x.png"
+        appendChild(des, ensemble)
+        appendChild(image, des)
+        appendChild(description, des)
+
+        // date et heure 
+        let dayHour = document.createElement('p')
+        dayHour.classList.add("dayandhour")
+        dayHour.textContent = weekday[new Date(elem.dt_txt).getDay()] + " " + elem.dt_txt.substring(11, 13) + "h"
+        appendChild(dayHour, ensemble)
+
+        //temperature
+        let divTemperature = document.createElement("div")
+        addClassName(divTemperature, "temperature")
+        let temperature = document.createElement("p")
+        temperature.classList.add("temperatureactuelle")
+        temperature.textContent = "🌡️ " + ((elem.main.temp).toFixed(0) + "°")
+        appendChild(divTemperature, ensemble)
+        appendChild(temperature, divTemperature)
+
+        //temperature min/max
+        let tempsMinMax = document.createElement("p")
+        addClassName(tempsMinMax, "temperatureminmax")
+        tempsMinMax.textContent = (elem.main.temp_min).toFixed(0) + "°" + "/" + ((elem.main.temp_max).toFixed(0) + "°")
+        appendChild(tempsMinMax, divTemperature)
+
+        //div reste
+        let reste = document.createElement("div")
+        addClassName(reste, "reste")
+        appendChild(reste, ensemble)
+
+        //ressenti
+        let feels = document.createElement("p")
+        addClassName(feels, "ressenti")
+        feels.textContent = "Feels like : " + (elem.main.feels_like).toFixed(0) + "°"
+        appendChild(feels, reste)
         
-        let divCarte= document.createElement("div")
-            divCarte.classList.add("carte")
-            div.appendChild(divCarte)
+        // vitesse du vent 
+        let vVent=document.createElement("p")
+        addClassName(vVent,"vent")
+        vVent.textContent= "💨 "+((elem.wind.speed)*3.6).toFixed(0)+ " km/h "
+        appendChild(vVent,reste)
 
-        for (elem of list )
-        {
-           
-            let ensemble=document.createElement("div")
-            ensemble.classList.add(elem.dt_txt.split(" ")[0],"class"+elem.dt_txt.substring(11,13))
-            divCarte.appendChild(ensemble)
+        // pourcentage d'humidité
+        let humidity= document.createElement("p")
+        addClassName(humidity,"humidite")
+        humidity.textContent= "💧 " +elem.main.humidity + "%"
+        appendChild(humidity,reste)
 
-            //image
-            let des=document.createElement("div")
-            des.classList.add("image")
-            let description=document.createElement("p")
-            description.classList.add("description")
-            description.textContent= elem.weather[0].description
-            let image=document.createElement("img")
-            image.src="http://openweathermap.org/img/wn/"+elem.weather[0].icon+"@2x.png"
-            ensemble.appendChild(des)
-            des.appendChild(image)
-            des.appendChild(description)
-            
-            // date et heure 
-            let dayHour= document.createElement('p')
-            dayHour.classList.add("dayandhour")
-            dayHour.textContent= weekday[new Date(elem.dt_txt).getDay()]
-            ensemble.appendChild(dayHour)
-
-            //temperature
-            let divTemp=document.createElement("div")
-            divTemp.classList.add("temperatures")
-            let temperature=document.createElement("p")
-            temperature.classList.add("temperatureactuelle")
-            temperature.textContent= "🌡️ "+((elem.main.temp).toFixed(0) + "°" )
-            ensemble.appendChild(divTemp)
-            divTemp.appendChild(temperature)
-
-            //temperature min/max
-            let tempsMinMax=document.createElement("p")
-            tempsMinMax.classList.add("temperatureMax")
-            tempsMinMax.textContent= (elem.main.temp_min).toFixed(0) + "°"+ "/" +((elem.main.temp_max).toFixed(0) + "°")
-            divTemp.appendChild(tempsMinMax) 
-
-            //div reste
-            let reste=document.createElement("div")
-            reste.classList.add("reste")
-            ensemble.appendChild(reste)
-
-            //ressenti
-            let feels=document.createElement("p")
-            feels.classList.add("ressenti")
-            feels.textContent= "Feels like : "+(elem.main.feels_like).toFixed(0) +"°"
-            reste.appendChild(feels)
-
-            // vitesse du vent 
-            let vVent=document.createElement("p")
-            vVent.classList.add("vitessevent")
-            vVent.textContent= "💨 "+((elem.wind.speed)*3.6).toFixed(0)+ " km/h "
-            reste.appendChild(vVent)
-
-            // pourcentage d'humidité
-            let hum= document.createElement("p")
-            hum.classList.add("humidité")
-            hum.textContent= "💧 " +elem.main.humidity + "%"
-            reste.appendChild(hum)
-
-            //affichage carte uniquement midi
-
-            if (ensemble.classList.contains("class12"))
-            {
-                arrDay.push(weekday[new Date(elem.dt_txt).getDay()])
-                arrTemp.push((elem.main.temp).toFixed(0))
-                arrFeels.push((elem.main.feels_like).toFixed(0))
-            }
-            else
-            {
-                ensemble.style.display="none"
-            }
-        }
+        arrDay.push(weekday[new Date(elem.dt_txt).getDay()])
+        arrTemp.push((elem.main.temp).toFixed(0))
+        arrFeels.push((elem.main.feels_like).toFixed(0))
+    }
     })
-        document.body.insertBefore(div,divGraph)
-        const barCanvas= document.querySelector("canvas");
-        const barChart = new Chart(barCanvas, 
+
+    document.body.insertBefore(div,divGraph)
+    const barCanvas= document.querySelector("canvas");
+    const barChart = new Chart(barCanvas, 
     {
         type:"line",
         data: 
@@ -138,10 +137,7 @@ button.addEventListener('click',()=>
                     'red',
                 ],
                 pointBorderWidth: 3,
-
             }]
-
-         }
-         
-    })
+        }
+    }) 
 })
